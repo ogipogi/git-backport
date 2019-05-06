b1d79e7c56c6976aeafdb8160369ea445c70f686
PJ Hampton <pjhampton@protonmail.com>
2019-05-06 21:32:55 +0100

    add help info.

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+const Version = "0.0.1"
+
+func PrintInfo() {
+	fmt.Printf("git backport :: v%s\n\n", Version)
+
+	fmt.Println("HOW TO >>>>>")
+	fmt.Println("$ git backport commit_hash:branch_name")
+	fmt.Println("$ git backport commit_hash:branch_name1,branch_name2,branch_name3")
+	fmt.Println("<<<<<")
+}
+
 func main()  {
-	fmt.Println("this is git backport")
+	PrintInfo()
 }
