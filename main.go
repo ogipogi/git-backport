cb889501d075243a3ba3af168277cc9431f93ca9
PJ Hampton <pjhampton@protonmail.com>
2019-05-06 21:47:31 +0100

    finish help screen.

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import "fmt"
 const Version = "0.0.1"
 
 func PrintInfo() {
-	fmt.Printf("git backport :: v%s\n\n", Version)
+	fmt.Printf("\ngit backport :: v%s\n\n", Version)
 
-	fmt.Println("HOW TO >>>>>")
+	fmt.Printf("HOW TO >>>>>\n")
 	fmt.Println("$ git backport commit_hash:branch_name")
 	fmt.Println("$ git backport commit_hash:branch_name1,branch_name2,branch_name3")
-	fmt.Println("<<<<<")
+	fmt.Printf("<<<<<\n\n")
 }
 
 func main()  {
