92dac73c7132065917ce03e5558216cee13dff28
PJ Hampton <pjhampton@protonmail.com>
2019-05-15 21:26:18 +0100

    refactor the get branches func.

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os/exec"
 )
 
-const Version = "0.0.6"
+const Version = "0.0.7"
 
 type BackportOp struct {
 	hash string
@@ -16,11 +16,7 @@ type BackportOp struct {
 
 func main()  {
 	args := os.Args
-	var (
-		cmdOut []byte
-		err    error
-		gitBranches []string
-	)
+	var gitBranches []string
 
 	if len(args) < 2 {
 		PrintManual()
@@ -35,15 +31,7 @@ func main()  {
 		return
 	}
 
-	cmdName := "git"
-	cmdArgs := []string{"branch", "-a"}
-	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		PrintManual()
-		os.Exit(1)
-	}
-
-	branches := strings.Split(strings.TrimSpace(string(cmdOut)), "\n")
+	branches := GetBranches()
 	for _, element := range branches {
 		branch := strings.Replace(element, "*", "", 1)
 		branch = strings.Replace(branch, "remotes/origin/", "", 1)
@@ -51,6 +39,33 @@ func main()  {
 	}
 
 	CheckIfBranchesExist(backportInfo.branches, gitBranches)
+}
+
+func PrintManual() {
+	fmt.Printf("\ngit backport :: v%s\n\n", Version)
+
+	fmt.Printf("HOW TO >>>>>\n")
+	fmt.Println("$ git backport commit_hash:branch_name")
+	fmt.Println("$ git backport commit_hash:branch_name1,branch_name2,branch_name3")
+	fmt.Printf("<<<<<\n\n")
+}
+
+func GetBranches() []string {
+	var (
+		cmdOut []byte
+		err error
+	)
+
+	cmdOp := "git"
+	cmdArgs := []string{"branch", "-a"}
+
+	if cmdOut, err = exec.Command(cmdOp, cmdArgs...).Output(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		PrintManual()
+		os.Exit(1)
+	}
+
+	return strings.Split(strings.TrimSpace(string(cmdOut)), "\n")
 }
 
 func CheckIfBranchesExist(branches []string, gitBranches []string) {
@@ -63,15 +78,6 @@ func CheckIfBranchesExist(branches []string, gitBranches []string) {
 			os.Exit(1)
 		}
 	}
-}
-
-func PrintManual() {
-	fmt.Printf("\ngit backport :: v%s\n\n", Version)
-
-	fmt.Printf("HOW TO >>>>>\n")
-	fmt.Println("$ git backport commit_hash:branch_name")
-	fmt.Println("$ git backport commit_hash:branch_name1,branch_name2,branch_name3")
-	fmt.Printf("<<<<<\n\n")
 }
 
 func GetHashAndBranches(input string) BackportOp {
